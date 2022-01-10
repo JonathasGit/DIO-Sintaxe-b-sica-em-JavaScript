e3f1cb55be26d64773c0efdab92863f1cc339d27
JonathasGit <jonathas-25@hotmail.com>
2022-01-09 22:36:20 -0200

    #1 Entendendo variáveis e seus valores - Tipos de variáveis

---
+++ aula 2 variaveis/script.js
@@ -0,0 +1,19 @@
+//Tipos Primitivos
+
+//boleanos
+var vOuF = false;
+console.log(vOuF);
+//descobrindo o tipo  primitivo da variavel 
+console.log(typeof(vOuF));
+
+// number
+
+var numeroQUalquer = 1;
+console.log(numeroQUalquer);
+console.log(typeof(numeroQUalquer));
+
+//String
+
+var nome = 'Jonathas';
+console.log(nome);
+console.log(typeof(nome));
