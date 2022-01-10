31f021cecf5fa3cd98b1dc63c4b8fe742c5e9433
JonathasGit <jonathas-25@hotmail.com>
2022-01-10 01:09:09 -0200

    #1 4 - Vetores e Objetos - Manipulando os Arrays

--- Aula 3 - Arrays e Objetos/script3.js
+++ Aula 3 - Arrays e Objetos/script3.js
@@ -8,3 +8,26 @@ console.log(array);
 let array2 = ['string', 1, true, ['arrayTeste1'], ['arrayTeste2'], ['arrayTeste3'], ['arrayTeste4'] ]
 console.log(array2);
 console.log(array2[4]); // Pegar um determinado array ali de cima,
+
+//Manipulando os Arrays;
+//for each forEach() – itera um array;
+array.forEach(function(item,index){console.log(item, index)});
+
+//push() – add item no final do array;
+array.push('Novo item');
+console.log(array);
+
+//pop() – remove item no final do array;
+array2.pop();
+console.log(array2); // tem que remover o ultimo array arrayTeste4
+
+//shift() – remove item no início do array;
+array2.shift();
+console.log(array2); // tem que remover o primeiro array Sting
+
+//unshift() – add item no início do array;
+array2.unshift('Novo item no inicio');
+console.log(array2);
+
+//indexOf() – retorna o índice de um valor;
+console.log(array2.indexOf(true));
