52ebd702070f66bd855f8716ce8e0bc7c7b9bdd2
JonathasGit <jonathas-25@hotmail.com>
2022-01-09 23:59:49 -0200

    #1

--- Aula 3 - Arrays e Objetos/script3.js
+++ Aula 3 - Arrays e Objetos/script3.js
@@ -1,1 +1,10 @@
-// O que são Vetores e Arrays
+// O que são Vetores ou Arrays
+
+// Como declarar um array
+let array = ['string', 1, true];
+console.log(array); 
+
+//pode guardar qualquer tipo de dados.
+let array2 = ['string', 1, true, ['arrayTeste1'], ['arrayTeste2'], ['arrayTeste3'], ['arrayTeste4'] ]
+console.log(array2);
+console.log(array2[4]); // Pegar um determinado array ali de cima,
