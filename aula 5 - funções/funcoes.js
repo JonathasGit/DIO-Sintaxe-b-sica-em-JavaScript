bd015c06696030290a5b4ae65dee5bbc67dbadb3
JonathasGit <jonathas-25@hotmail.com>
2022-01-12 19:00:48 -0200

    #1 6 -  funcao

---
+++ aula 5 - funções/funcoes.js
@@ -0,0 +1,12 @@
+// Declaração de função     
+function funcao() {
+    console.log('Imprimir a mensagem - Essa mensagem veio de uma função')
+}
+//chamada da função
+funcao();
+
+function mensagem (primeiro, segundo){
+    console.log(primeiro, segundo);
+}
+
+mensagem('Primeiro valor - Tudo certo', 'Segundo valor - Jovem!');
