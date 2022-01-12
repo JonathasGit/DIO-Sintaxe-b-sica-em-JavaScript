ab077f153905b52649e1aecb8a11cacbc891555b
JonathasGit <jonathas-25@hotmail.com>
2022-01-12 13:35:03 -0200

    #1 5 - fazendo verificacao

--- aula 4 - Estruturas condicionais/script4.js
+++ aula 4 - Estruturas condicionais/script4.js
@@ -4,12 +4,19 @@ var placar;
 
 //Alinhamento de if e colocar um if dentro do outro : 
 if (jogador1 != -1) { // Sinal de diferente
-if (jogador1 > 0){ //caso jogador marcar ponto
+    // Usando IF
+if (jogador1 > 0 && jogador2 == 0 ) { //caso jogador marcar ponto
 console.log('Jogador 1 marcou ponto!');
-} else if (jogador2 > 0) {
+placar = jogador1 > jogador2;
+} 
+// Usando  Else if
+else if (jogador2 > 0 && jogador1 == 0) { 
     console.log('Jogador 2 marcou ponto');
+    placar = jogador2 > jogador1; 
+
 }
 
+//Usando else
 else {
     console.log('Ninguém marcou ponto');
 }
