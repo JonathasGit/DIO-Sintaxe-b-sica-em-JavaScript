6dd0d346f4136afede3c8a44b0f9304b10f806b3
JonathasGit <jonathas-25@hotmail.com>
2022-01-12 13:27:05 -0200

    #1 5 - Subindo os if

--- aula 4 - Estruturas condicionais/script4.js
+++ aula 4 - Estruturas condicionais/script4.js
@@ -2,3 +2,18 @@ var jogador1 = 0;
 var jogador2 = 0;
 var placar; 
 
+//Alinhamento de if e colocar um if dentro do outro : 
+if (jogador1 != -1) { // Sinal de diferente
+if (jogador1 > 0){ //caso jogador marcar ponto
+console.log('Jogador 1 marcou ponto!');
+} else if (jogador2 > 0) {
+    console.log('Jogador 2 marcou ponto');
+}
+
+else {
+    console.log('Ninguém marcou ponto');
+}
+}
+
+//if iternário  
+jogador1 != -1 && jogador2 != -1 ? console.log('Os jogadores são válidos') : console.log('Jogadores Inválidos');
