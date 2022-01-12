bb65e067e28ea1e6550db59bcd62fa2728f9554a
JonathasGit <jonathas-25@hotmail.com>
2022-01-12 17:14:52 -0200

    #1 5 - While

--- aula 4 - Estruturas condicionais/EsturutaDeRepeticao.js
+++ aula 4 - Estruturas condicionais/EsturutaDeRepeticao.js
@@ -3,6 +3,8 @@ let array = ['valor1', 'valor2', 'valor3', 'valor4', 'valor5']
 
 let object = {propriedade1: 'valor1', propriedade2: 'valor2', propriedade3: 'valor3',}
 
+var a = 0; //
+
 // FOR - Executa uma instrução até que ela seja falsa 
 
 //Enquanto array não atinge o valor total de 5, indice receb mais 1 
@@ -32,3 +34,9 @@ for (i of object.propriedade1){
     console.log(i); // Não da para usar for of com objetos
 }
 
+
+// While
+while (a < 100) {
+    a++;
+     console.log(a);
+}
